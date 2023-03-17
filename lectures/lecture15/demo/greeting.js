d21150882d2f0d768f707d2869527f2d37fd53b5
G-Money <gmckee97@gmail.com>
2023-03-17 10:51:33 -0400

    quiz 2

---
+++ lectures/lecture15/demo/greeting.js
@@ -0,0 +1,7 @@
+function sayHello() {
+	document.querySelector('#message').innerHTML = '<p>fuk u!</p>';
+}
+
+function sayGoodbye() {
+	document.querySelector('#message').innerHTML = '<p>Goodbye!</p>';
+}
